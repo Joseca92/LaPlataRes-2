3293ef0820e0e8e9c0b9af1998141a57c113b768
artreve <sebas981@live.com>
2022-08-20 00:19:35 -0300

    creacion base de proyecto

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,18 @@
+//Aqui van las rutas de las paginas
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Nav from "../components/Nav";
+
+
+const RoutesDos = () => {
+  return (
+    <>
+    <Nav/>
+      <Routes>
+        
+      </Routes>
+    </>
+  );
+};
+
+export default RoutesDos;
