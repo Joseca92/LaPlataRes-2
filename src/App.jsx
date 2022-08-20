935e7e9d08191cd42c1fa7fc56535e87ee2a0484
Joseca <calamandrei94@gmail.com>
2022-08-19 23:53:58 -0300

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <div><h1>hola react</h1></div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/Pedido.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Pedido = () => {
+  return (
+    <div className="container">
+        <div className="row">
+            <div className="col">
+                <h3>Pedido</h3>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Pedido
