17a6188e64edb96919b43488bb98ad3c2e65dfc5
Joseca <calamandrei94@gmail.com>
2022-08-21 23:06:20 -0300

    ac

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomeScreen from "./pages/HomeScreen";
+import ErrorScreen from "./pages/ErrorScreen"
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
+import PedidoScreen from "./pages/PedidoScreen";
 function App() {
   return (
     <BrowserRouter>
       <Nav/>
       <Routes>
         <Route path='/home' element={<HomeScreen/>}/>
+        <Route path="/*" element={<ErrorScreen/>}/>
+        <Route path="/PedidoScreen" element={<PedidoScreen/>} />
       </Routes>
       <Footer/>
     </BrowserRouter>
