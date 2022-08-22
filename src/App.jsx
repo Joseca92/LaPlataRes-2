10d3e3e672efb40342f132e55661509c0f930bf5
Maria Agostina Albornoz <Agostinaalbornoz43@gmail.com>
2022-08-22 16:08:53 -0300

    hice la estructura del login y el registro

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomeScreen from "./pages/HomeScreen";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
+import LoginScreen from "./pages/LoginScreen";
+import RegistroScreen from "./pages/RegistroScreen";
 function App() {
   return (
     <BrowserRouter>
       <Nav/>
       <Routes>
         <Route path='/home' element={<HomeScreen/>}/>
+        <Route path="/login" element={<LoginScreen />} />
+        <Route path="/registro" element={<RegistroScreen/>} />
       </Routes>
       <Footer/>
     </BrowserRouter>
