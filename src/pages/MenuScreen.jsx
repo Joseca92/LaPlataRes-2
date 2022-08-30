93a3382c5c298d2e1e88b37096a6230b027dd5ef
María Agostina Albornoz <agostinaalbornoz43@gmail.com>
2022-08-30 11:54:17 -0300

    acomode un poco el codigo de menuscreen

--- src/pages/MenuScreen.jsx
+++ src/pages/MenuScreen.jsx
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from "react";
 import {getCategoria, postMenu} from "../helpers/fetchApp";
-//import ReactMarkdown from "react-markdown";
-
 
 const MenuScreen = () => {
   const[mensaje,setMensaje] = useState([])
@@ -12,14 +10,7 @@ const MenuScreen = () => {
   const[categoria,setCategoria]=useState({})
   const[categorias,setCategorias]=useState([])
   const[imagen,setImagen]=useState('')
-  
- /*  useEffect(()=>{
-    setEstadoMenu(['Disponible','No disponible'])
-     setEstadoMenuSelect(['Seleccione'])
-     setCategoria(['Menú del día','Postre'])
-     setCategoriaSelect(['Seleccione'])
-  },[]) */
-  
+   
   const handleSubmit = (e) => {
     e.preventDefault();
 
@@ -30,7 +21,6 @@ const MenuScreen = () => {
       detalle,
       precio,
       categoria,
-   
     };
 
     postMenu(datos).then((respuesta) => {
@@ -42,7 +32,6 @@ const MenuScreen = () => {
       }
       setNombre("");
       setEstadoMenu("");
-     ;
       setdetalle("");
       setEstadoMenu(true);
       setPrecio(0);
@@ -149,10 +138,6 @@ const MenuScreen = () => {
                 ))}
               </div>
             )}
-         {/*  <div>
-              <p>Preview</p>
-              <ReactMarkdown>{body}</ReactMarkdown>
-                  </div>*/}
           </div>
         </div>
       
@@ -180,92 +165,3 @@ const MenuScreen = () => {
 }
 
 export default MenuScreen
-/*
-export default function RegistrarMenu(){
-  const[nombre,setNombre]=useState=('')
-  const[estadoMenu,setEstadoMenu]=useState=([])
-  const[estadoMenuSelect,setEstadoMenuSelect]=useState=([1])
- const[detalle,setdetalle]=useState=('')
- const[precio,setPrecio]=useState=('')
-  const[categoria,setCategoria]=useState=([])
-  const[categoriaSelect,setCategoriaSelect]=useState-([])
- 
-  useEffect(()=>{
-   setEstadoMenu(['Disponible','No disponible'])
-    setEstadoMenuSelect('Seleccione')
-    setCategoria(['Menú del día','Postre'])
-    setCategoriaSelect('Seleccione')
- },[])
- 
- const guardar= async(e)=>{
-    e.preventDefault()
-     const usuario={
-      nombre,
-      estadoMenu:estadoMenuSelect, 
-      detalle,
-      precio,
-      categoria:categoriaSelect,
-        }
-
-      if(guardar===''){
-        Swal.fire({
-          icon:'error',
-          title:'debe escribir un nombre',
-          showconfirmButton:false,
-          timer:1500
-        })
-      }
-      else
-      {
-        const token = sessionStorage.getItem('token')
-        const respuesta = await Axios.post('/') //falta la ruta
-      }
-                           }                       I
-return(
-  <div className="container mt-4">
-    <div className="row">
-      <div className="col-md-7 mx-auto">
-       <div className="card">
-        <div className="conteiner text center fa-5x">
-          <i className="fas fa-user-plus"></i>
-        </div>
-        <div className="card-header bg-info text-center">
-          <h4>Crear nuevo menú</h4>
-        </div>
-        <div className="card-body">
-          <form onSubmit={"guardar"}>
-            <div className="col-md-6">
-              <label>Nombre</label>
-              <input type="text" className='form-control required' />
-            </div>
-
-            <div className="col-md-6">
-              <label>Estado</label>
-              <input type="text" className='form-control required' />
-            </div>
-
-            <div className="col-md-6">
-              <label>Detalle</label>
-              <input type="text" className='form-control required' />
-            </div>
-
-            <div className="col-md-6">
-              <label>Precio</label>
-              <input type="text" className='form-control required' />
-            </div>
-
-            <div className="col-md-6">
-              <label>Categoria</label>
-              <input type="text" className='form-control required' />
-            </div>
-            <br />
-            <button type="submit" className="btn btn-outline-info">
-              <span className="fa fa-save"></span> Crear nuevo menú
-            </button>
-          </form>
-        </div>
-       </div>
-      </div>
-    </div>
-  </div>
-*/
