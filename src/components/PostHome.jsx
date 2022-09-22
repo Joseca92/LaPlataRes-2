d698df2fe299afbbd4b818440c17d63b86c435e9
JuanMaParedes <juan.manuel.paredes@hotmail.com>
2022-09-22 01:57:58 -0300

    Buscador de menu HOME inProgress+1

--- src/components/PostHome.jsx
+++ src/components/PostHome.jsx
@@ -23,12 +23,12 @@ const PostHome = ( {post, handleClick}) => {
                         <p className="card-text">{detalle}</p>
                         <p className="card-text"> $ {precio}</p>
                       </div>
-                      <button
+                    {/*   <button
             
                          onClick={handleClick}
                          className="btnGral fw-bold p-2 mx-2">
                           Agregar
-                         </button>
+                         </button> */}
                       
                     </div>
                     
